ee4d961d755df99f3353dd9fa66c63812db36e6b
Richard Li Defa <richardlidefa@gmail.com>
2020-12-19 16:04:38 +0800

    create new task function

--- app/javascript/components/Tasks.jsx
+++ app/javascript/components/Tasks.jsx
@@ -5,12 +5,17 @@ class Tasks extends Component {
     constructor(props) {
         super(props);
         this.state = { 
-            tasks: []
+            tasks: [],
+            description: '',
+            category: ''
          }
 
          this.deleteTask = this.deleteTask.bind(this);
          this.renderTableData = this.renderTableData.bind(this);
+         this.handleChange = this.handleChange.bind(this);
+         this.handleSubmit = this.handleSubmit.bind(this);
     }
+    
 
     componentDidMount() {
         const url = "/api/v1/tasks/index";
@@ -41,7 +46,7 @@ class Tasks extends Component {
               if (response.ok) {
                 return response.json();
               }
-              throw new Error("Fk u.");
+              throw new Error("Something is wrong.");
             })
             .then(response => {
                 let updated = this.state.tasks.slice();
@@ -57,9 +62,9 @@ class Tasks extends Component {
                 const { description, category } = task //destructuring
                 return (
                 <tr key={index} className='taskTable tableBody'>
-                    <td>{description}</td>
                     <td>{category}</td>
-                    <td><button value={task.id} onClick={this.deleteTask}>Delete</button></td>
+                    <td>{description}</td>
+                    <td><button value={task.id} onClick={this.deleteTask}>Done</button></td>
                 </tr>
                 )     
         })
@@ -68,18 +73,68 @@ class Tasks extends Component {
      renderTableHeader() {
         return (
             <tr className='taskTable tableHeader'>
-                <td>Description</td>
                 <td>Category</td>
+                <td>Description</td>
                 <td></td>
             </tr>
         )
      }
 
+     stripHtmlEntities(str) {
+        return String(str)
+          .replace(/</g, "&lt;")
+          .replace(/>/g, "&gt;");
+      }
+
+     handleSubmit(event) {
+        event.preventDefault();
+        const url = "/api/v1/tasks/create";
+        const { description, category } = this.state;
+    
+        if (description.length == 0 || category.length == 0)
+          return;
+    
+        const body = {
+          description,
+          category,
+        };
+    
+        const token = document.querySelector('meta[name="csrf-token"]').content;
+        fetch(url, {
+          method: "POST",
+          headers: {
+            "X-CSRF-Token": token,
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify(body)
+        })
+          .then(response => {
+            if (response.ok) {
+              return response.json();
+            }
+            throw new Error("Network response was not ok.");
+          })
+          .then(response => {
+            this.componentDidMount();
+            this.setState({
+                description: '',
+                category: ''})
+          })
+          .catch(error => console.log(error.message));
+      }
+
+     handleChange(event) {
+        this.setState({ 
+            [event.target.name] : event.target.value 
+          })
+     }
+
+
     render() { 
         const { tasks } = this.state;
         const allTasks = (
             <div>
-                <table id='tasks'>
+                <table className='tasks'>
                     <tbody>
                         {this.renderTableHeader()}
                         {this.renderTableData(tasks)}
@@ -94,29 +149,36 @@ class Tasks extends Component {
           );
         return ( 
             <>
-                {tasks.length > 0 ? allTasks : noTask}
-                <form className='center d-flex'>
+                <form className='center d-flex' onSubmit={this.handleSubmit}>
                     <label className='formEle'>
-                    Description: 
-                    <input
-                        name="description"
-                        value={this.state.going}
-                        onChange={this.handleInputChange} />
+                        Category: 
+                        <input
+                            className='catInput'
+                            name="category"
+                            placeholder='Category'
+                            value={this.state.category}
+                            onChange={this.handleChange}
+                        />
                     </label>
                     <label className='formEle'>
-                    Category: 
-                    <input
-                        name="category"
-                        value={this.state.numberOfGuests}
-                        onChange={this.handleInputChange} />
+                        Description: 
+                        <input
+                            className='desInput'
+                            name="description"
+                            placeholder='New Description'
+                            value={this.state.description}
+                            onChange={this.handleChange}
+                        />
                     </label>
                     <label className='formEle'>
                         <input type='submit' value='Submit' />
                     </label>
                 </form>
+                {tasks.length > 0 ? allTasks : noTask}
             </>
          );
     }
 }
+
  
 export default Tasks;
